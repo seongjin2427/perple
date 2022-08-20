89e5d58f7f27de647d76018513adc3eb04335abb
seongjin <seongjin2427@gmail.com>
2022-08-21 00:10:05 +0900

    comment, like and views fetch function

---
+++ front/src/components/shared/Pagination/Pagination.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 2rem;
+`;
+
+export const PrevNextButton = styled.button`
+  width: 8rem;
+  height: 4rem;
+  margin: 1rem;
+`;
--- front/src/components/shared/YoutubeList/YoutubeList.tsx
+++ front/src/components/shared/YoutubeList/YoutubeList.tsx
@@ -1,23 +1,44 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import * as S from './YoutubeList.styled';
 import { getPopularVideos, GetPopularVideosType } from 'api/youtube';
+import Pagination from 'components/shared/Pagination';
+import abbreviateNumber from 'util/abbreviate';
 
 const YoutubeList = () => {
   const [videos, setVideos] = useState<GetPopularVideosType | undefined>();
+  const [statistics, setStatistics] = useState<
+    GetPopularVideosType | undefined
+  >();
+
+  const getVideos = useCallback(
+    async (token?: string) => {
+      const fetchedVideos: GetPopularVideosType | undefined =
+        await getPopularVideos({ divide: 'snippet', token });
+
+      const fetchedStatistics: GetPopularVideosType | undefined =
+        await getPopularVideos({
+          divide: 'statistics',
+          token,
+        });
+
+      setVideos(fetchedVideos);
+      setStatistics(fetchedStatistics);
+
+      window.scrollTo({ top: 0 });
+    },
+    [setVideos],
+  );
 
   useEffect(() => {
-    (async function () {
-      const data: GetPopularVideosType | undefined = await getPopularVideos();
-      setVideos(data);
-    })();
-  }, []);
+    getVideos();
+  }, [getVideos]);
 
   return (
     <S.Container>
       <S.Title>인기 동영상</S.Title>
       <S.VideoListDiv>
-        {videos?.items.map((item) => (
+        {videos?.items.map((item, idx) => (
           <S.VideoWrapper key={item.id}>
             <S.VideoThumbnailDiv>
               <S.VideoThumbnail src={item.snippet.thumbnails.high.url} />
@@ -31,9 +52,37 @@ const YoutubeList = () => {
                 {item.snippet.localized.description}
               </S.VideoDescription>
             </S.VideoTextArea>
+            {statistics?.items[idx].id === item.id && (
+              <S.VideoStatisticsArea>
+                <S.VideoStatisticsParagraph>
+                  댓글 {' / '}
+                  {abbreviateNumber(
+                    statistics?.items[idx].statistics.commentCount,
+                  )}
+                </S.VideoStatisticsParagraph>
+                <S.VideoStatisticsParagraph>
+                  좋아요 {' / '}
+                  {abbreviateNumber(
+                    statistics?.items[idx].statistics.likeCount,
+                  )}
+                </S.VideoStatisticsParagraph>
+                <S.VideoStatisticsParagraph>
+                  조회 {' / '}
+                  {abbreviateNumber(
+                    statistics?.items[idx].statistics.viewCount,
+                  )}
+                </S.VideoStatisticsParagraph>
+              </S.VideoStatisticsArea>
+            )}
           </S.VideoWrapper>
         ))}
       </S.VideoListDiv>
+      <Pagination
+        prevPageToken={videos?.prevPageToken}
+        nextPageToken={videos?.nextPageToken}
+        pageInfo={videos?.pageInfo}
+        getVideos={getVideos}
+      />
     </S.Container>
   );
 };
